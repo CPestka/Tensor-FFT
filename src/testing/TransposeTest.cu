c200ea92d022b18e14fd5555442189b39782e707
CPestka <constantin.pestka@web.de>
2021-07-04 14:33:32 +0200

    added Unit test file

--- src/testing/TransposeTest.cu
+++ src/testing/TransposeTest.cu
@@ -6,60 +6,74 @@
 #include <cuda_runtime.h>
 #include <cuda.h>
 
-#include "../base/Plan.cpp"
-#include "../base/ComputeFFT.cu"
 #include "TestingDataCreation.cu"
-#include "../base/Timer.h"
 #include "FileWriter.cu"
 
-int main(){
+bool transpose16_test(){
   int fft_length = 16*16*16;
-  int amount_of_r16_steps = 2;
-  int amount_of_r2_steps = 0;
-  int transpose_blocksize = 256;
 
   std::vector<float> weights;
   weights.push_back(1.0);
-  //weights.push_back(1.4);
-  std::unique_ptr<__half[]> data = CreateSineSuperpostion(fft_length, weights);
+  std::unique_ptr<__half[]> data_1 =
+      CreateSineSuperpostion(fft_length, weights).get();
+  std::unique_ptr<__half[]> data_2 =
+      CreateSineSuperpostion(fft_length, weights).get();
 
-  WriteResultsToFile("input.dat", fft_length, data.get());
+  WriteResultsToFile("input.dat", fft_length, data_1.get());
 
-  std::unique_ptr<__half[]> transposed_data =
-      std::make_unique<__half[]>(2 * fft_length);
+  __half old1_RE[16*16][16];
+  __half old1_IM[16*16][16];
+  __half new1_RE[16][16*16];
+  __half new1_IM[16][16*16];
 
-  for(int i=0; i<fft_length*2; i++){
-    transposed_data[i] = data[i];
-  }
-
-  __half tmp_RE[16][16][16];
-  __half tmp_IM[16][16][16];
-  for(int i=0; i<16; i++){
+  for(int i=0; i<16*16; i++){
     for(int j=0; j<16; j++){
-      for(int k=0; k<16; k++){
-        tmp_RE[k][j][i] = transposed_data[i + 16*j + 16*16*k];
-        tmp_RE[k][j][i] = transposed_data[i + 16*j + 16*16*k + fft_length];
-      }
-    }
-  }
-  __half tmp1_RE[16][16][16];
-  __half tmp1_IM[16][16][16];
-
-  //do the transposes i.e. from x[i][j][k] to x[k][j][i]
-  for(int i=0; i<16; i++){
-    for(int j=0; j<16; j++){
-      for(int k=0; k<16; k++){
-        tmp1_RE[k][j][i] = tmp_RE[i][k][j];
-        tmp1_IM[k][j][i] = tmp_IM[i][k][j];
-        transposed_data[i + 16*j + 16*16*k] = tmp1_RE[k][j][i];
-        transposed_data[i + 16*j + 16*16*k + fft_length] = tmp1_IM[k][j][i];
-      }
+      old1_RE[i][j] = data_2[j + 16 * i];
+      old1_IM[i][j] = data_2[j + 16 * i + fft_length];
     }
   }
 
-  WriteResultsToFile("transposed_test.dat", fft_length, transposed_data.get());
+  for(int i=0; i<16; i++){
+    for(int j=0; j<16*16; j++){
+      new1_RE[i][j] = old1_RE[j][i];
+      new1_IM[i][j] = old1_IM[j][i];
+    }
+  }
 
-  //Allocation of in/out data arrays on the device
+  __half old2_RE[16][16][16];
+  __half old2_IM[16][16][16];
+  __half new2_RE[16][16][16];
+  __half new2_IM[16][16][16];
+
+  for(int i=0; i<16; i++){
+    for(int j=0; j<16; j++){
+      for(int k=0; k<16; k++){
+        old2_RE[i][j][k] = new1_RE[i][k + 16 * j];
+        old2_IM[i][j][k] = new1_IM[i][k + 16 * j];
+      }
+    }
+  }
+
+  for(int i=0; i<16; i++){
+    for(int j=0; j<16; j++){
+      for(int k=0; k<16; k++){
+        new2_RE[i][j][k] = old2_RE[i][k][j];
+        new2_IM[i][j][k] = old2_IM[i][k][j];
+      }
+    }
+  }
+
+  for(int i=0; i<16; i++){
+    for(int j=0; j<16; j++){
+      for(int k=0; k<16; k++){
+        data_2[k + 16*j + 16*16*i] = new2_RE[i][j][k];
+        data_2[k + 16*j + 16*16*i + fft_length] = new2_IM[i][j][k];
+      }
+    }
+  }
+
+  WriteResultsToFile("transposed_test_cpu.dat", fft_length, data_2.get());
+
   __half* dptr_input_RE;
   __half* dptr_input_IM;
   __half* dptr_results_RE;
@@ -70,10 +84,10 @@ int main(){
   dptr_results_RE = dptr_input_IM + fft_length;
   dptr_results_IM = dptr_results_RE + fft_length;
 
-  //Memcpy of input data to device
-  cudaMemcpy(dptr_input_RE, data.get(), 2 * fft_length * sizeof(__half),
-                 cudaMemcpyHostToDevice);
+  cudaMemcpy(dptr_input_RE, data_1.get(), 2 * fft_length * sizeof(__half),
+             cudaMemcpyHostToDevice);
 
+  int transpose_blocksize = 256;
   int amount_of_transpose_blocks =
      ceil(static_cast<float>(fft_length) /
           static_cast<float>(transpose_blocksize));
@@ -81,11 +95,190 @@ int main(){
   //radix steps
   TransposeKernel<<<amount_of_transpose_blocks, transpose_blocksize>>>(
       dptr_input_RE, dptr_input_IM, dptr_results_RE, dptr_results_IM,
-      fft_length, amount_of_r16_steps, amount_of_r2_steps);
+      fft_length, 2, 0);
 
   //Memcpy of input data to device
-  cudaMemcpy(data.get(), dptr_input_RE, 2 * fft_length * sizeof(__half),
+  cudaMemcpy(data_1.get(), dptr_input_RE, 2 * fft_length * sizeof(__half),
                  cudaMemcpyDeviceToHost);
 
-  WriteResultsToFile("transposed_kernel.dat", fft_length, data.get());
+  WriteResultsToFile("transposed_test_kernel.dat", fft_length, data_1.get());
+
+  for(int i=0; i<fft_length; i++){
+    if ((data_1[i] != data_2[i]) ||
+        (data_1[i + fft_length] != data_2[i + fft_length])){
+      std::cout << "Diferent results on CPU and GPU!" << std::endl;
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool transpose16_2_test(){
+  int fft_length = 16*16*16*2*2;
+
+  std::vector<float> weights;
+  weights.push_back(1.0);
+  std::unique_ptr<__half[]> data_1 =
+      CreateSineSuperpostion(fft_length, weights).get();
+  std::unique_ptr<__half[]> data_2 =
+      CreateSineSuperpostion(fft_length, weights).get();
+
+  WriteResultsToFile("input.dat", fft_length, data_1.get());
+
+  __half old1_RE[16*16*16*2][2];
+  __half old1_IM[16*16*16*2][2];
+  __half new1_RE[2][2*16*16*16];
+  __half new1_IM[2][2*16*16*16];
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<16*16*16*2; j++){
+      old1_RE[j][i] = data_2[i + 2*j];
+      old1_IM[j][i] = data_2[i + 2*j + fft_length];
+    }
+  }
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<16*16*16*2; j++){
+      new1_RE[i][j] = old1_RE[j][i];
+      new1_IM[i][j] = old1_IM[j][i];
+    }
+  }
+
+  __half old2_RE[2][16*16*16][2];
+  __half old2_IM[2][16*16*16][2];
+  __half new2_RE[2][2][16*16*16];
+  __half new2_IM[2][2][16*16*16];
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<16*16*16; j++){
+      for(int k=0; k<2; k++){
+        old2_RE[i][j][k] = new1_RE[i][k + 2 * j];
+        old2_IM[i][j][k] = new1_IM[i][k + 2 * j];
+      }
+    }
+  }
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+      for(int k=0; k<16*16*16; k++){
+        new2_RE[i][j][k] = old2_RE[i][k][j];
+        new2_IM[i][j][k] = old2_IM[i][k][j];
+      }
+    }
+  }
+
+  __half old3_RE[2][2][16*16][16];
+  __half old3_IM[2][2][16*16][16];
+  __half new3_RE[2][2][16][16*16];
+  __half new3_IM[2][2][16][16*16];
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+      for(int k=0; k<16*16; k++){
+        for(int l=0; l<16; l++){
+          old3_RE[i][j][k][l] = new2_RE[i][j][l + 16 * k];
+          old3_IM[i][j][k][l] = new2_IM[i][j][l + 16 * k];
+        }
+      }
+    }
+  }
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+      for(int k=0; k<16; k++){
+        for(int l=0; l<16*16; l++){
+          new3_RE[i][j][k][l] = old3_RE[i][j][l][k];
+          new3_IM[i][j][k][l] = old3_IM[i][j][l][k];
+        }
+      }
+    }
+  }
+
+  __half old4_RE[2][2][16][16][16];
+  __half old4_IM[2][2][16][16][16];
+  __half new4_RE[2][2][16][16][16];
+  __half new4_IM[2][2][16][16][16];
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+      for(int k=0; k<16; k++){
+        for(int l=0; l<16; l++){
+          for(int m=0; m<16; m++){
+            old4_RE[i][j][k][l][m] = new3_RE[i][j][k][m + 16 * l];
+            old4_IM[i][j][k][l][m] = new3_IM[i][j][k][m + 16 * l];
+          }
+        }
+      }
+    }
+  }
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+      for(int k=0; k<16; k++){
+        for(int l=0; l<16; l++){
+          for(int m=0; m<16; m++){
+            new4_RE[i][j][k][l][m] = old4_RE[i][j][k][m][l];
+            new4_IM[i][j][k][l][m] = old4_IM[i][j][k][m][l];
+          }
+        }
+      }
+    }
+  }
+
+  for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+      for(int k=0; k<16; k++){
+        for(int l=0; l<16; l++){
+          for(int m=0; m<16; m++){
+            data_2[m + 16*l + 16*16*k + 16*16*16*j + 16*16*16*2*i] =
+                new4_RE[i][j][k][l][m];
+            data_2[m + 16*l + 16*16*k + 16*16*16*j + 16*16*16*2*i + fft_length]
+                = new4_IM[i][j][k][l][m];
+          }
+        }
+      }
+    }
+  }
+
+  WriteResultsToFile("transposed_test_cpu.dat", fft_length, data_2.get());
+
+  __half* dptr_input_RE;
+  __half* dptr_input_IM;
+  __half* dptr_results_RE;
+  __half* dptr_results_IM;
+  cudaMalloc((void**)(&dptr_input_RE), 4 * sizeof(__half) * fft_length);
+
+  dptr_input_IM = dptr_input_RE + fft_length;
+  dptr_results_RE = dptr_input_IM + fft_length;
+  dptr_results_IM = dptr_results_RE + fft_length;
+
+  cudaMemcpy(dptr_input_RE, data_1.get(), 2 * fft_length * sizeof(__half),
+             cudaMemcpyHostToDevice);
+
+  int transpose_blocksize = 256;
+  int amount_of_transpose_blocks =
+     ceil(static_cast<float>(fft_length) /
+          static_cast<float>(transpose_blocksize));
+  //Launch kernel that performs the transposes to prepare the data for the
+  //radix steps
+  TransposeKernel<<<amount_of_transpose_blocks, transpose_blocksize>>>(
+      dptr_input_RE, dptr_input_IM, dptr_results_RE, dptr_results_IM,
+      fft_length, 2, 2);
+
+  //Memcpy of input data to device
+  cudaMemcpy(data_1.get(), dptr_input_RE, 2 * fft_length * sizeof(__half),
+                 cudaMemcpyDeviceToHost);
+
+  WriteResultsToFile("transposed_test_kernel.dat", fft_length, data_1.get());
+
+  for(int i=0; i<fft_length; i++){
+    if ((data_1[i] != data_2[i]) ||
+        (data_1[i + fft_length] != data_2[i + fft_length])){
+      std::cout << "Diferent results on CPU and GPU!" << std::endl;
+      return false;
+    }
+  }
+
+  return true;
 }
---
+++ src/testing/UnitTest.cu
@@ -0,0 +1,44 @@
+//Used to test functonality
+#include <iostream>
+#include <string>
+
+#include "TransposeTest.cu"
+
+int main(){
+  if (!transpose16_test()){
+    std::cout << "Transpose 16*16*16 test failed!" << std::endl;
+    return;
+  }
+  if (!transpose16_2_test()){
+    std::cout << "Transpose 16*16*16*2*2 test failed!" << std::endl;
+    return;
+  }
+
+  /*
+  if (!dft_16_test()){
+    std::cout << "DFT kernel test failed for 16*16*16!" << std::endl;
+    return;
+  }
+  if (!dft_16_2_test()){
+    std::cout << "DFT kernel test failed for 16*16*16*2*2!" << std::endl;
+    return;
+  }
+
+  if (!r16_16_test()){
+    std::cout << "R16 kernel test failed for 16*16*16!" << std::endl;
+    return;
+  }
+  if (!r16_16_2_test()){
+    std::cout << "R16 kernel test failed for 16*16*16*2*2!" << std::endl;
+    return;
+  }
+
+
+  if (!r2_test()){
+    std::cout << "R2 kernel test failed for 16*16*16*2*2!" << std::endl;
+    return;
+  }
+  */
+
+  std::cout << "All tests passed!" <<std::endl;
+}
