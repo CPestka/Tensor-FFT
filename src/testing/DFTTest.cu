40807d9041fbee56dfdd3eefe82ae68a2775d325
CPestka <constantin.pestka@web.de>
2021-07-14 13:47:19 +0200

    added dft kernel test for simple example data

--- src/testing/DFTTest.cu
+++ src/testing/DFTTest.cu
@@ -74,6 +74,64 @@ bool dft_0_test(){
   return true;
 }
 
+bool dft_sin_test(){
+  std::unique_ptr<__half[]> data_RE =
+      std::make_unique<__half[]>(16*16*16);
+  std::unique_ptr<__half[]> data_IM =
+      std::make_unique<__half[]>(16*16*16);
+
+  for(int j=0; j<16*16; j++){
+    for(int i=0; i<16; i++){
+      data_RE[j*16 + i] = sin((2*M_PI*i)/16.0);
+      data_IM[j*16 + i] = 0;
+    }
+  }
+
+
+  __half* dptr_data_RE;
+  __half* dptr_data_IM;
+  __half* dptr_results_RE;
+  __half* dptr_results_IM;
+  cudaMalloc((void**)(&dptr_data_RE), sizeof(__half)*16*16*16);
+  cudaMalloc((void**)(&dptr_data_IM), sizeof(__half)*16*16*16);
+  cudaMalloc((void**)(&dptr_results_RE), sizeof(__half)*16*16*16);
+  cudaMalloc((void**)(&dptr_results_IM), sizeof(__half)*16*16*16);
+
+  cudaMemcpy(dptr_data_RE, data_RE.get(), 16*16*16*sizeof(__half),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(dptr_data_IM, data_IM.get(), 16*16*16*sizeof(__half),
+             cudaMemcpyHostToDevice);
+
+  __half* dptr_dft_matrix_RE;
+  __half* dptr_dft_matrix_IM;
+  cudaMalloc((void**)(&dptr_dft_matrix_RE), sizeof(__half)*16*16*16);
+  cudaMalloc((void**)(&dptr_dft_matrix_IM), sizeof(__half)*16*16*16);
+
+  ComputeDFTMatrix<<<16,16*16>>>(dptr_dft_matrix_RE, dptr_dft_matrix_IM);
+
+  DFTKernel<<<1,32>>>(dptr_data_RE, dptr_data_IM, dptr_results_RE,
+                      dptr_results_IM, dptr_dft_matrix_RE, dptr_dft_matrix_IM);
+
+  cudaMemcpy(data_RE.get(), dptr_results_RE, 16*16*16*sizeof(__half),
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(data_IM.get(), dptr_results_IM, 16*16*16*sizeof(__half),
+             cudaMemcpyDeviceToHost);
+
+  cudaDeviceSynchronize();
+
+  WriteResultsToFile("dft_sin_test.dat", 16*16*16, data_RE.get(),
+                     data_IM.get());
+
+  cudaFree(dptr_dft_matrix_IM);
+  cudaFree(dptr_dft_matrix_RE);
+  cudaFree(dptr_results_IM);
+  cudaFree(dptr_results_RE);
+  cudaFree(dptr_data_IM);
+  cudaFree(dptr_data_RE);
+
+  return true;
+}
+
 /*
 __global__ void PrepareCuFFTInput(__half* input_RE, __half* input_IM,
                                   __half2* cuFFT_in){
