8c0885863858eb0ee4f2221ee8e7c612ce74e36b
CPestka <constantin.pestka@web.de>
2021-08-02 13:44:54 +0200

    debugging

--- src/testing/Tuner.cu
+++ src/testing/Tuner.cu
@@ -79,8 +79,8 @@ double GetAverageExecutionTime(int fft_length, int warmup_samples,
 }
 
 int main(){
-  int log_length_max = 16;
-  int sample_size = 25;
+  int log_length_max = 24;
+  int sample_size = 100;
   int warmup_samples = 5;
   int warp_max_cap = 32;
 
