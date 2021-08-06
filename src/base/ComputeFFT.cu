b6c5ad6147f2e895a6637d2cd883bb173cacf579
CPestka <constantin.pestka@web.de>
2021-08-06 16:12:31 +0200

    debugging

--- src/base/ComputeFFT.cu
+++ src/base/ComputeFFT.cu
@@ -176,7 +176,7 @@ std::optional<std::string> ComputeFFTAlt(AltPlan &fft_plan, AltDataHandler &data
 
 
   cudaLaunchCooperativeKernel(TensorFFT, gridDim, blockDim, &(args[0]),
-                              fft_shared_mem_amount);
+                              static_cast<size_t>(fft_shared_mem_amount));
 
   /*
   int sub_fft_length = 16;
