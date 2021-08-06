40f7f58b3344a338df8f8500a41fbef41b9a0d23
CPestka <constantin.pestka@web.de>
2021-08-06 14:19:24 +0200

    debugging

--- src/base/FFT.cu
+++ src/base/FFT.cu
@@ -193,15 +193,14 @@ __global__ void TensorFFT(__half* input_data_RE, __half* input_data_IM,
   //Perform the matrix multiplication of two complex matrices AxB via 4 matrix
   //multiplications i.e. RE(AxB)=RE(A)xRE(B) - IM(A)xIM(B) and IM(AxB) =
   //RE(A)xIM(B) + IM(A)xRE(B)
-  wmma::mma_sync(accumulator_RE_1_frag, dft_RE_frag, data_RE_frag,
+  wmma::mma_sync(accumulator_RE_1_frag, data_RE_frag, dft_RE_frag,
                  accumulator_RE_1_frag);
-  wmma::mma_sync(accumulator_RE_2_frag, dft_IM_frag, data_IM_frag,
+  wmma::mma_sync(accumulator_RE_2_frag, data_IM_frag, dft_IM_frag,
                  accumulator_RE_2_frag);
-  wmma::mma_sync(accumulator_IM_frag, dft_IM_frag, data_RE_frag,
+  wmma::mma_sync(accumulator_IM_frag, data_IM_frag, dft_RE_frag,
                  accumulator_IM_frag);
-  wmma::mma_sync(accumulator_IM_frag, dft_RE_frag, data_IM_frag,
+  wmma::mma_sync(accumulator_IM_frag, data_RE_frag, dft_IM_frag,
                  accumulator_IM_frag);
-
 
   //Store results to buffer
   wmma::store_matrix_sync(buffer_IM, accumulator_IM_frag, 16,
