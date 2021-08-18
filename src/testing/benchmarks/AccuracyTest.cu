3fb922ff48f61b12c766d4da27d8dc8b22beb60d
CPestka <constantin.pestka@web.de>
2021-08-18 14:46:25 +0200

    Debugging

--- src/testing/benchmarks/AccuracyTest.cu
+++ src/testing/benchmarks/AccuracyTest.cu
@@ -10,7 +10,7 @@
 
 #include "../AccuracyCalculator.h"
 #include "../unitTesting/CuFFTTest.h"
-#include "../unitTesting/FTTest.h"
+#include "../unitTesting/FTTTest.h"
 #include "../../base/ComputeFFT.h"
 #include "../FileWriter.h"
 
