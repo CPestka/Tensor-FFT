a71342ccd42ec0db9508338423df069f3ee36e84
CPestka <constantin.pestka@web.de>
2021-08-18 12:51:13 +0200

    Debugging

--- src/testing/ExampleSingleFFT.cu
+++ src/testing/ExampleSingleFFT.cu
@@ -34,7 +34,7 @@ int main(){
   //The plan holds parameters needed for the execution of the kernels which are
   //mostly derived from the fft length.
   std::optional<Plan<int>> possible_plan = CreatePlan(fft_length);
-  Plan my_plan;
+  Plan<int> my_plan;
   if (possible_plan) {
     my_plan = possible_plan.value();
   } else {
