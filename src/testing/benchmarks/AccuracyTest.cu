eeea07e04cf15bd1f332c4d1a7d715f0c60c005e
CPestka <constantin.pestka@web.de>
2021-08-20 14:47:11 +0200

    debugging

--- src/testing/benchmarks/AccuracyTest.cu
+++ src/testing/benchmarks/AccuracyTest.cu
@@ -32,38 +32,38 @@ int main(){
     std::string cuFFT_file_name = ("accuracy_cuFFT_" +
                                    std::to_string(fft_length.back()))
                                   + ".dat";
-    //err = CreateComparisonDataDouble(fft_length.back(), cuFFT_file_name);
-    if (err) {
-      std::cout << err.value() << std::endl;
-      return false;
-    }
+    // err = CreateComparisonDataDouble(fft_length.back(), cuFFT_file_name);
+    // if (err) {
+    //   std::cout << err.value() << std::endl;
+    //   return false;
+    // }
 
-    std::vector<std::string> fft_results_file_names;
-    for(int k=0; k<sample_size; k++){
-      fft_results_file_names.push_back(((("accuracy_" +
-                                        std::to_string(fft_length.back())) +
-                                        "_") + std::to_string(k)) + ".dat");
-      err = FullSingleFFTComputation(fft_length.back(),
-                                     fft_results_file_names.back());
-      if (err) {
-        std::cout << err.value() << std::endl;
-        return false;
-      }
-    }
-
-    avg_dev.push_back(ComputeAverageDeviation(fft_results_file_names,
-                                              cuFFT_file_name));
-    sigma_of_dev.push_back(ComputeSigmaOfDeviation(fft_results_file_names,
-                                                   cuFFT_file_name,
-                                                   avg_dev.back()));
-  }
-
-  err = WriteAccuracyTestResultsToFile(avg_dev, sigma_of_dev, sample_size,
-                                       fft_length);
-  if (err) {
-    std::cout << err.value() << std::endl;
-    return false;
-  }
+  //   std::vector<std::string> fft_results_file_names;
+  //   for(int k=0; k<sample_size; k++){
+  //     fft_results_file_names.push_back(((("accuracy_" +
+  //                                       std::to_string(fft_length.back())) +
+  //                                       "_") + std::to_string(k)) + ".dat");
+  //     err = FullSingleFFTComputation(fft_length.back(),
+  //                                    fft_results_file_names.back());
+  //     if (err) {
+  //       std::cout << err.value() << std::endl;
+  //       return false;
+  //     }
+  //   }
+  //
+  //   avg_dev.push_back(ComputeAverageDeviation(fft_results_file_names,
+  //                                             cuFFT_file_name));
+  //   sigma_of_dev.push_back(ComputeSigmaOfDeviation(fft_results_file_names,
+  //                                                  cuFFT_file_name,
+  //                                                  avg_dev.back()));
+  // }
+  //
+  // err = WriteAccuracyTestResultsToFile(avg_dev, sigma_of_dev, sample_size,
+  //                                      fft_length);
+  // if (err) {
+  //   std::cout << err.value() << std::endl;
+  //   return false;
+  // }
 
   return true;
 }
