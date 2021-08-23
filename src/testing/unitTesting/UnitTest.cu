8ac1b8ec17be1daf4ff21db97d6a3e0a2fd170d1
CPestka <constantin.pestka@web.de>
2021-08-23 19:19:15 +0200

    debugging

--- src/testing/unitTesting/UnitTest.cu
+++ src/testing/unitTesting/UnitTest.cu
@@ -24,23 +24,23 @@ int main(){
   }
 
   int fft_length = start_fft_length;
-  int j = 0;
   while (fft_length <= end_fft_length) {
-    if (!TestFullFFT(fft_length,
-                     average_deviation_threshold,
-                     sigma_deviation_threshold,
-                     max_deviation_threshold,
-                     weights_RE[j],
-                     weights_IM[j])) {
-      std::cout << "Error! Test at fft_length: "
-                << fft_length
-                << " failed!"
-                << std::endl;
-      return false;
+    for(int j=0; j<runs_per_fft_length; j++){
+      if (!TestFullFFT(fft_length,
+                       average_deviation_threshold,
+                       sigma_deviation_threshold,
+                       max_deviation_threshold,
+                       weights_RE[j],
+                       weights_IM[j])) {
+        std::cout << "Error! Test at fft_length: "
+                  << fft_length
+                  << " failed!"
+                  << std::endl;
+        return false;
+      }
     }
 
     fft_length = (fft_length * 2);
-    j++;
   }
 
   std::cout << "All tests passed!" <<std::endl;
