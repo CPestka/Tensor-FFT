ea053c8fad71d4dc5c8d9928260eabd4bf39c114
CPestka <constantin.pestka@web.de>
2021-08-24 14:09:50 +0200

    testing

--- src/testing/CuFFTExample.cu
+++ src/testing/CuFFTExample.cu
@@ -17,13 +17,13 @@ int main(){
 
   std::vector<float> weights_RE { 1.0, 0.7, 0.5, 0.2, 0.3, 0.7, 0.8 };
   std::vector<float> weights_IM { 1.0, 0.3, 0.2, 0.4, 0.9, 0.1, 0.6 };
-  std::unique_ptr<__half2[]> data =
-      CreateSineSuperpostionH2(fft_length, weights_RE, weights_IM);
+  std::unique_ptr<cufftComplex[]> data =
+      CreateSineSuperpostionF2(fft_length, weights_RE, weights_IM);
 
-  __half2* dptr_data;
-  __half2* dptr_results;
-  cudaMalloc(&dptr_data, sizeof(__half2) * fft_length);
-  cudaMalloc(&dptr_results, sizeof(__half2) * fft_length);
+  cufftComplex* dptr_data;
+  cufftComplex* dptr_results;
+  cudaMalloc(&dptr_data, sizeof(cufftComplex) * fft_length);
+  cudaMalloc(&dptr_results, sizeof(cufftComplex) * fft_length);
 
   cufftHandle plan;
   cufftResult r;
@@ -35,33 +35,33 @@ int main(){
   }
 
   size_t size = 0;
-  r = cufftXtMakePlanMany(plan, 1, &fft_length, nullptr, 1, 1,
-                          CUDA_C_16F, nullptr, 1, 1, CUDA_C_16F, 1, &size,
-                          CUDA_C_16F);
-  //r = cufftPlan1d(&plan, fft_length, CUFFT_C2C, 0);
+  // r = cufftXtMakePlanMany(plan, 1, &fft_length, nullptr, 1, 1,
+  //                         CUDA_C_16F, nullptr, 1, 1, CUDA_C_16F, 1, &size,
+  //                         CUDA_C_16F);
+  r = cufftPlan1d(&plan, fft_length, CUFFT_C2C, 0);
 
   if (r != CUFFT_SUCCESS) {
     std::cout << "Error! Plan creation failed." << std::endl;
     return false;
   }
 
-  cudaMemcpy(dptr_data, data.get(), fft_length * sizeof(__half2),
+  cudaMemcpy(dptr_data, data.get(), fft_length * sizeof(cufftComplex),
              cudaMemcpyHostToDevice);
 
-  r = cufftXtExec(plan, dptr_data, dptr_results, CUFFT_FORWARD);
-  //r = cufftExecC2C(plan, dptr_data, dptr_results, CUFFT_FORWARD);
+  // r = cufftXtExec(plan, dptr_data, dptr_results, CUFFT_FORWARD);
+  r = cufftExecC2C(plan, dptr_data, dptr_results, CUFFT_FORWARD);
   if (r != CUFFT_SUCCESS) {
     std::cout << "Error! Plan execution failed." << std::endl;
     return false;
   }
 
-  cudaMemcpy(data.get(), dptr_results, fft_length * sizeof(__half2),
+  cudaMemcpy(data.get(), dptr_results, fft_length * sizeof(cufftComplex),
              cudaMemcpyDeviceToHost);
 
   cudaDeviceSynchronize();
 
   //Write results to file
-  WriteResultsToFileHalf2("example_results.dat", fft_length, data.get());
+  WriteResultsToFileFloat2("example_results.dat", fft_length, data.get());
 
   cufftDestroy(plan);
   cudaFree(dptr_results);
