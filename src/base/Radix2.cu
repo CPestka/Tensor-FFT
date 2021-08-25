11bd4138681c9a1027ca1fc8704cff0903b440a9
CPestka <constantin.pestka@web.de>
2021-08-25 16:41:03 +0200

    testing

--- src/base/Radix2.cu
+++ src/base/Radix2.cu
@@ -31,23 +31,20 @@ __global__ void Radix2Kernel(__half* input_data_RE, __half* input_data_IM,
   //sub_fft_length
   float tmp = static_cast<float>(memory_point1_offset) /
               static_cast<float>(sub_fft_length);
-  //Modulo version for higher accuracy?
-  // tmp float = static_cast<float>(memory_point1_offset % (sub_fft_length * 2)) /
-  //             static_cast<float>(sub_fft_length);
   __half phase = __hmul(static_cast<__half>(M_PI), static_cast<__half>(tmp));
 
   __half twiddle_RE = hcos(phase);
   __half twiddle_IM = -hsin(phase);
 
   //Fetch current data once from global memory to use it twice
-  __half input_RE = input_data_RE[memory_point2_offset];
-  __half input_IM = input_data_IM[memory_point2_offset];
+  __half point2_RE = input_data_RE[memory_point2_offset];
+  __half point2_IM = input_data_IM[memory_point2_offset];
 
   //Multiply point 2 with twiddle factor
   __half modified_point2_RE =
-      __hsub(__hmul(input_RE, twiddle_RE), __hmul(input_IM, twiddle_IM));
-  __half modified_point2_IM = 
-      __hfma(input_RE , twiddle_IM, __hmul(input_IM, twiddle_RE));
+      __hsub(__hmul(point2_RE, twiddle_RE), __hmul(point2_IM, twiddle_IM));
+  __half modified_point2_IM =
+      __hfma(point2_RE , twiddle_IM, __hmul(point2_IM, twiddle_RE));
 
   //Load point 1 from global mem once to use it twice
   __half point1_RE = input_data_RE[memory_point1_offset];
@@ -76,4 +73,6 @@ __global__ void Radix2Kernel(__half* input_data_RE, __half* input_data_IM,
       __hmul(__hsub(point1_RE, modified_point2_RE), static_cast<__half>(0.5));
   output_data_IM[memory_point2_offset] =
       __hmul(__hsub(point1_IM, modified_point2_IM), static_cast<__half>(0.5));
+
+  printf("ID: %d tmp: %f phase: %f twid_RE: %f twid_IM %f p1_RE: %f p1_IM: %f p2_RE: %f p2_IM: %f p2Mod_RE: %f p2Mod_IM: %f p1Out_RE: %f p1Out_IM: %f p2Out_RE: %f p2Out_IM: %f", memory_point1_offset, tmp, static_cast<float>(phase), static_cast<float>(twiddle_RE), static_cast<float>(twiddle_IM), static_cast<float>(point1_RE), static_cast<float>(point1_IM), static_cast<float>(point2_RE), static_cast<float>(point2_IM), static_cast<float>(output_data_RE[memory_point1_offset]), static_cast<float>(output_data_IM[memory_point1_offset]), static_cast<float>(output_data_RE[memory_point2_offset]), static_cast<float>(output_data_RE[memory_point2_offset]));
 }
