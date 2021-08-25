b3e38bd04f164b075493ca1f1060527827ec1e69
CPestka <constantin.pestka@web.de>
2021-08-25 18:05:06 +0200

    testing

--- src/testing/R2Test.cu
+++ src/testing/R2Test.cu
@@ -19,10 +19,19 @@ int main(){
 
   WriteResultsToFile("r2_in.dat", fft_length, data.get());
 
+  std::unique_ptr<__half[]> preped_data =
+      std::make_unique<__half[]>(2 * fft_length);
+
+  for(int i=0; i<fft_length/2; i++){
+    preped_data[i] = data[2 * i];
+    preped_data[i + (fft_length / 2)] = data[(2 * i) + 1];
+  }
+  WriteResultsToFile("r2_preped.dat", fft_length, preped_data.get());
+
   __half* dptr_data;
   cudaMalloc(&dptr_data, sizeof(__half) * 4 * fft_length);
 
-  cudaMemcpy(dptr_data, data.get(),
+  cudaMemcpy(dptr_data, preped_data.get(),
              sizeof(__half) * 2 * fft_length, cudaMemcpyHostToDevice);
 
   __half* data_RE = dptr_data;
