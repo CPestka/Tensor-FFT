12cc695147fe1df222ebddf46c51045f1116c320
CPestka <constantin.pestka@web.de>
2021-08-31 17:02:46 +0200

    testing

--- src/testing/benchmarks/FFTBenchSinlge.cu
+++ src/testing/benchmarks/FFTBenchSinlge.cu
@@ -9,10 +9,10 @@
 
 int main(){
   constexpr int start_fft_length = 16*16;
-  constexpr int end_fft_length = 16*16*16*16*16 * 16*2;
+  constexpr int end_fft_length = 16*16*16*16*16 * 16*16*2;
 
   constexpr int sample_size = 500;
-  constexpr int warmup_samples = 10;
+  constexpr int warmup_samples = 20;
 
   std::vector<int> fft_length;
   fft_length.push_back(start_fft_length);
