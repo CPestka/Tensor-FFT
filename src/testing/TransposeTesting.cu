570895abe459298fa7f6eb067b95164f33abe7e7
CPestka <constantin.pestka@web.de>
2021-09-06 14:46:23 +0200

    testing transpose alternatives

---
+++ src/testing/TransposeTesting.cu
@@ -0,0 +1,13 @@
+
+
+
+int main() {
+  int fft_length = 16*16*16*16*16 * 16*2;
+  __half2* dptr_data;
+  __half2* dptr_results;
+
+  cudaMalloc(&dptr_data, 2 * sizeof(__half2) * fft_length);
+  dptr_results = dptr_data + fft_length;
+
+  TransposeKernel<<fft_length/512, 512>>(dptr_data, dptr_results, fft_length, 5, 1);
+}
