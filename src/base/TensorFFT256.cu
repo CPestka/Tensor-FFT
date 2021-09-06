26d9dbdc58a984cdda487166b1001d60d1e5ad7f
CPestka <constantin.pestka@web.de>
2021-09-06 14:37:54 +0200

    testing

--- src/base/TensorFFT256.cu
+++ src/base/TensorFFT256.cu
@@ -53,23 +53,23 @@ __global__ void TensorFFT256(__half* input_data_RE, __half* input_data_IM,
   //On the fly computation of DFT matrix
   //TODO: test speed and accuracy of cos,cosf,coh (and modulo version of those)
   //and literal version
-  // #pragma unroll
-  // for(int k=0; k<8; k++){
-  //   int j = k + 8 * inter_warp_id_is_upper_16;
-  //   int buffer_array_id = inter_warp_id_16 + 16 * j;
-  //
-  //   // __half phase =
-  //   //     __hdiv(__hmul(static_cast<__half>(j * inter_warp_id_16),
-  //   //                   static_cast<__half>(M_PI)),
-  //   //            static_cast<__half>(8.0));
-  //   float phase = (static_cast<float>(j * inter_warp_id_16) * M_PI) / 8.0;
-  //
-  //   buffer_RE[buffer_array_id] = cosf(phase);
-  //   buffer_IM[buffer_array_id] = -sinf(phase);
-  // }
+  #pragma unroll
+  for(int k=0; k<8; k++){
+    int j = k + 8 * inter_warp_id_is_upper_16;
+    int buffer_array_id = inter_warp_id_16 + 16 * j;
+
+    // __half phase =
+    //     __hdiv(__hmul(static_cast<__half>(j * inter_warp_id_16),
+    //                   static_cast<__half>(M_PI)),
+    //            static_cast<__half>(8.0));
+    float phase = (static_cast<float>(j * inter_warp_id_16) * M_PI) / 8.0;
+
+    buffer_RE[buffer_array_id] = cosf(phase);
+    buffer_IM[buffer_array_id] = -sinf(phase);
+  }
 
   //Literal version of dft matrix.
-  LoadLiteralDFTMatrixToShared(inter_warp_id, buffer_RE, buffer_IM);
+  //LoadLiteralDFTMatrixToShared(inter_warp_id, buffer_RE, buffer_IM);
 
   //Load DFT matrix into the according fragments
   wmma::load_matrix_sync(dft_RE_frag, buffer_RE, 16);
