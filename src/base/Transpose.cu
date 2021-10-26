6d0173fbf2a7072ba61fdbec3591199496694acd
CPestka <constantin.pestka@web.de>
2021-10-26 17:37:55 +0200

    debugging

--- src/base/Transpose.cu
+++ src/base/Transpose.cu
@@ -9,25 +9,39 @@
 //data to the shuffled postion and copying it accordingly results in terrible
 //performace as the output postions are scattered far appart for a linear
 //sequence of input elements, or vice versa.
-//This approach each block reads in 64 initialy linear sequences of 64 elements
+//In this approach each block reads 64 initialy linear sequences of 64 elements
 //each, that have the particular stride that 1 element from each sequence form
 //1 linear 64 sequence for the output, resulting in a total of 64 length 64
 //sequences to write back to gloabl memory. Thus the intial and final global
 //memory accesses are 256Byte large (element size is 4Byte), which is optimal
-//and the shuffling is performed in significantly faster shared memory.
+//and the shuffling is performed in the significantly faster shared memory.
 //ONLY WORKS FOR 16 warps per block i.e. blocksize=512!!! (should be best for
 //performance anyways)
 template <typename Integer>
 __global__ void Transposer(__half2* input_data, __half2* output_data,
                            Integer fft_length, int amount_of_r16_steps,
                            int amount_of_r2_steps){
+  //The thread id within a block maps to the id within a row [0:63] and to the
+  //id within the 64 specific rows needed for the shuffel
   int inter_block_row_id = threadIdx.x / 64;
   int inter_row_id = threadIdx.x % 64; //[0,7]
 
+  //stride of elments in input data that are linear in second to last dim of
+  //output data
   Integer small_row_stride = fft_length / 256;
+  //stride of elments in input data that are linear in last dim of output data
   Integer large_row_stride = fft_length / 16;
 
+  //As within one block 64 rows from the intital memory are taken, such that
+  //when one takes one specific element from each one receives a sequence that
+  //is linear in the output and the last two dimension of the output array are
+  //always of size 16, the linear sequence streatches across all of the last and
+  //across 1/4 of the second to last dimension. To simplify the index
+  //calculation, 4 sequential blocks are bundled together to fully streatch
+  //the second to last dim.
   int second_to_last_dim_quadrant_id = blockIdx.x % 4;
+
+  //Data offset per bundle of 4 blocks is 1 row of 64 elements
   Integer data_offset = (blockIdx.x / 4) * 64;
 
   //Numbering of the name of the shared mem buffer is in different kernels is
@@ -84,4 +98,35 @@ __global__ void Transposer(__half2* input_data, __half2* output_data,
     output_data[current_offset] = output_buffer[threadIdx.x + (512 * i)];
   }
 
+}
+
+//Old transpose kernel for testing (but with half2 instead of half)
+__global__ void TransposeKernel(__half2* input_data,
+                                __half2* output_data,
+                                int fft_length,
+                                int amount_of_r16_steps,
+                                int amount_of_r2_steps) {
+  //The thread id is the id for the entry of the input array we wish to store to
+  //the correct position in the output array
+  int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+  int output_id = 0;
+  int current_row_length = fft_length;
+  int tmp = id;
+
+  for(int i=0; i<amount_of_r2_steps; i++){
+    current_row_length = current_row_length / 2;
+    output_id += ((tmp % 2) * current_row_length);
+    tmp = tmp / 2;
+  }
+
+  for(int i=0; i<amount_of_r16_steps; i++){
+    current_row_length = current_row_length / 16;
+    output_id += ((tmp % 16) * current_row_length);
+    tmp = tmp / 16;
+  }
+  output_id += tmp;
+
+  //Move input data to correct position
+  output_data[output_id] = input_data[id];
 }
---
+++ src/testing/TransposeTest.cu
@@ -0,0 +1,111 @@
+//Example of usage of the FFT implementation with synthetic data
+#include <iostream>
+#include <string>
+#include <optional>
+#include <memory>
+
+#include "../base/Plan.h"
+#include "../base/ComputeFFT.h"
+#include "WeightMaker.h"
+#include "DataMaker.cu"
+#include "FileWriter.h"
+
+int main(){
+  int fft_length = 16*16*16*16;
+
+  //Get weights for data creation
+  //int amount_of_frequencies = 10;
+  int amount_of_frequencies = 5;
+  std::unique_ptr<float2[]> weights =
+      std::make_unique<float2[]>(amount_of_frequencies);
+  //SetRandomWeights(weights.get(), amount_of_frequencies, 42*42);
+  SetDummyWeightsRE1(weights.get());
+
+  float2* dptr_weights = nullptr;
+  cudaMalloc(&dptr_weights, sizeof(float2) * amount_of_frequencies);
+  cudaMemcpy(dptr_weights, weights.get(),
+             sizeof(float2) * amount_of_frequencies, cudaMemcpyHostToDevice);
+
+  //Needed if data set smaller than 64KB and can be removed otherwise.
+  cudaDeviceSynchronize();
+
+  //Allocate device memory
+  __half2* dptr_input_data = nullptr;
+  __half2* dptr_output_data = nullptr;
+  cudaMalloc(&dptr_input_data, 2 * sizeof(__half2) * fft_length);
+  dptr_output_data = dptr_input_data + fft_length;
+
+  //Allocate mem on host for results
+  std::unique_ptr<__half2[]> results = std::make_unique<__half2[]>(fft_length);
+
+  //Produce input data based on weights
+  SineSupperposition<int,__half2><<<fft_length / 1024, 1024>>>(
+      fft_length, dptr_input_data, dptr_weights, amount_of_frequencies);
+
+  //Needed if data set smaller than 64KB and can be removed otherwise.
+  cudaDeviceSynchronize();
+
+  if (cudaMemcpy(results.get(),
+                 dptr_input_data,
+                 fft_length * sizeof(__half2),
+                 cudaMemcpyDeviceToHost)
+       != cudaSuccess) {
+     std::cout << cudaGetErrorString(cudaPeekAtLastError()) << std::endl;
+     return false;
+  }
+
+  //Needed if data set smaller than 64KB and can be removed otherwise.
+  cudaDeviceSynchronize();
+
+  //Write results to file
+  WriteFFTToFile<__half2>("example_input.dat", fft_length, results.get());
+
+  Transposer<int><<<fft_length / 4096, 512, 32768>>>(
+      dptr_input_data, dptr_output_data, fft_length, 3, 0);
+
+  //Needed if data set smaller than 64KB and can be removed otherwise.
+  cudaDeviceSynchronize();
+
+  //Copy results back
+  if (cudaMemcpy(results.get(),
+                 dptr_output_data,
+                 fft_length * sizeof(__half2),
+                 cudaMemcpyDeviceToHost)
+       != cudaSuccess) {
+     std::cout << cudaGetErrorString(cudaPeekAtLastError()) << std::endl;
+     return false;
+  }
+
+  //Make sure the results have finished cpying
+  cudaDeviceSynchronize();
+
+  //Write results to file
+  WriteFFTToFile<__half2>("example_trans.dat", fft_length, results.get());
+
+  TransposeKernel<<<fft_length / 512, 512>>>(
+      dptr_input_data, dptr_output_data, fft_length, 3, 0);
+
+  //Needed if data set smaller than 64KB and can be removed otherwise.
+  cudaDeviceSynchronize();
+
+  //Copy results back
+  if (cudaMemcpy(results.get(),
+                 dptr_output_data,
+                 fft_length * sizeof(__half2),
+                 cudaMemcpyDeviceToHost)
+       != cudaSuccess) {
+     std::cout << cudaGetErrorString(cudaPeekAtLastError()) << std::endl;
+     return false;
+  }
+
+  //Make sure the results have finished cpying
+  cudaDeviceSynchronize();
+
+  //Write results to file
+  WriteFFTToFile<__half2>("example_trans_old.dat", fft_length, results.get());
+
+  cudaFree(dptr_weights);
+  cudaFree(dptr_input_data);
+
+  return true;
+}
