68f7f7a7eb717cc545c5d6512e83b2e6e6bb8a2f
CPestka <constantin.pestka@web.de>
2021-11-04 14:56:46 +0100

    debugging

--- src/base/TensorFFT4096_2.cu
+++ src/base/TensorFFT4096_2.cu
@@ -167,10 +167,12 @@ __global__ void TensorFFT4096_2(__half* input_data_RE, __half* input_data_IM,
     // buffer_IM[buffer_array_id] = input_data_IM[input_array_id];
 
     //For sequential scaling
-    buffer_RE[buffer_array_id] = __hdiv(input_data_RE[input_array_id],
-                                       static_cast<__half>(4096.0));
-    buffer_IM[buffer_array_id] = __hdiv(input_data_IM[input_array_id],
-                                       static_cast<__half>(4096.0));
+    // buffer_RE[buffer_array_id] = __hdiv(input_data_RE[input_array_id],
+    //                                    static_cast<__half>(4096.0));
+    // buffer_IM[buffer_array_id] = __hdiv(input_data_IM[input_array_id],
+    //                                    static_cast<__half>(4096.0));
+    buffer_RE[buffer_array_id] = input_data_RE[input_array_id];
+    buffer_IM[buffer_array_id] = input_data_IM[input_array_id];
 
     //For scaling in one step
     //buffer_RE[buffer_array_id] = __hdiv(input_data_RE[input_array_id],
