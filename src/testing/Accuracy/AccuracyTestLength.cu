46e62e6b082e10acb7c8bb4dbd245aedc31a4c73
CPestka <constantin.pestka@web.de>
2021-12-12 15:41:44 +0100

    testing

--- src/testing/Accuracy/AccuracyTestLength.cu
+++ src/testing/Accuracy/AccuracyTestLength.cu
@@ -71,12 +71,12 @@ int main(){
         GetNormalizationFactor<int>(normalize_to.back(), dptr_weights,
                                     amount_of_frequencies, fft_lengths.back());
     normalization_factors.push_back(normalization_factor);
-    errors.push_back(ComputeFP32VsFp64Errors(static_cast<int>(fft_lengths.back()),
+    errors.push_back(ComputeOurVsFp64Errors<int>(static_cast<int>(fft_lengths.back()),
         dptr_weights, amount_of_frequencies, normalization_factor));
     amount_of_frequencies_vec.push_back(amount_of_frequencies);
   }
 
-  WriteAccuracyToFile("AccTest_fp32_N_nu_256k.dat", normalize_to, fft_lengths, errors,
+  WriteAccuracyToFile("AccTest_our_N_nu_256k.dat", normalize_to, fft_lengths, errors,
                       amount_of_frequencies_vec, normalization_factors);
 
   cudaFree(dptr_weights);
