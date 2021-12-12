43bb0e200873a5d26155fae7c0bdf676b83418f0
CPestka <constantin.pestka@web.de>
2021-12-12 15:26:24 +0100

    testing

--- src/testing/Accuracy/ComparisonDataCuFFT.cu
+++ src/testing/Accuracy/ComparisonDataCuFFT.cu
@@ -163,7 +163,7 @@ std::unique_ptr<__half2[]> GetComparisionFP16Data(
 
   cudaDeviceSynchronize();
 
-  ScaleResults<long long, __half2>(fft_length, data.get());
+  //ScaleResults<long long, __half2>(fft_length, data.get());
 
   cufftDestroy(plan);
   cudaFree(dptr_data);
